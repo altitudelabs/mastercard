e483e89afc6326a16b124f6f7b72df3f46bac52c
Altitude Labs <altitudelabs@Altitudes-MacBook-Pro.local>
2015-03-20 16:17:23 +0800

    Add testing files

---
+++ MasterCard/TestingViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestingViewController.h
+//  MasterCard
+//
+//  Created by Altitude Labs on 20/3/15.
+//  Copyright (c) 2015 MasterCard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestingViewController : UIViewController
+
+@end
---
+++ MasterCard/TestingViewController.m
@@ -0,0 +1,37 @@
+//
+//  TestingViewController.m
+//  MasterCard
+//
+//  Created by Altitude Labs on 20/3/15.
+//  Copyright (c) 2015 MasterCard. All rights reserved.
+//
+
+#import "TestingViewController.h"
+
+@interface TestingViewController ()
+
+@end
+
+@implementation TestingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
